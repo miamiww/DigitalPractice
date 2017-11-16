81c767aebf4b04981f316e0a240da071f292f0aa
miamiworldwide <rivendalejones@gmail.com>
2017-11-16 15:02:16 -0500

    semi done

--- Week4/app.js
+++ Week4/app.js
@@ -1,16 +1,51 @@
+var json2csv = require('json2csv');
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var server = app.listen(3000);
-
 var io = require('socket.io')(server);
-
+var fields = ["name","time"];
+var newLine= "\r\n";
+var dataBase = "public/names.csv";
 
 app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+ 
 
-function _emitter(data){
-  io.sockets.emit('mysocket', data);
-  //console.log(data);
+io.on('connection', function(socket) {
+    console.log('A user connected');
+    socket.on('nameSocket',function(data){
+      console.log("connected");
+      console.log(data);
+      _csvWriter(dataBase,data);
+    })
+    socket.on('disconnect', function () {
+       console.log('A user disconnected');
+    });
+
+});
+
+
+function _csvWriter(csvName,data){
+  var toCSV = { data: data, fields: fields, hasCSVColumnTitle: false };
+  fs.stat(csvName, function(err, stat) {
+      if(err == null) {
+        console.log('File exists');
+        var csv = json2csv(toCSV) + newLine;
+        console.log(csv);
+        fs.appendFile(csvName, csv, function (err) {
+          if (err) throw err;
+          console.log('The "data to append" was appended to file!');
+        })
+      } else{
+        console.log('New file, just writing headers');
+        fields= (fields + newLine);
+        fs.writeFile(csvName, fields, function (err, stat) {
+          if (err) throw err;
+          console.log('file saved');
+        });
+      }
+  });
 }
--- Week4/public/sketch.js
+++ Week4/public/sketch.js
@@ -1,16 +1,87 @@
-let capture;
+var nameData = JSON.parse(localStorage.nameData || null) || {};
+var socket = io.connect(window.location.origin);
+var tableNames;
 
 
 function setup(){
- createCanvas(400,400);
- capture = createCapture(VIDEO);
- capture.size(width,height);
- capture.hide();
-
+  createCanvas(400, 400);
+  input = createInput();
+  input.position(20, 65);
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+  greeting = createElement('h2', 'who are you?');
+  greeting.position(20, 5);
+  textAlign(CENTER);
+  textSize(50);
 }
 
 function draw(){
-  image(capture,0,0);
-  // capture.loadPixels();
-  // background(220);
+  background(255);
+  // text(loadStuff(), width/2,height/2);
+
+}
+
+function greet() {
+  var name = input.value();
+  greeting.html('hello '+name+'! whats your email');
+  input.value('');
+  saveStuff(name);
+  button.remove();
+  nextButton = createButton("good job");
+  nextButton.position(input.x + input.width, 65);
+  nextButton.mousePressed(journey);
+}
+
+function journey(){
+  var newText = input.value();
+  greeting.html("still here?");
+  input.value('');
+  saveStuff(newText);
+  nextButton.remove();
+  unholyButton = createButton("clikc please");
+  unholyButton.position(input.x + input.width, 65);
+  unholyButton.mousePressed(showAll);
+}
+
+function showAll(){
+  unholyButton.remove();
+  greeting.remove();
+  input.remove();
+  _dataLoader("names.csv");
+  lastButton = createButton("see friends");
+  lastButton.position(10,10);
+  lastButton.mousePressed(printOut);
+}
+
+function printOut(){
+  lastButton.remove();
+  console.log(tableNames.getColumn("name"));
+  for(var r = 0; r < tableNames.getRowCount(); r++){
+    text(tableNames.getString(r,0),10,10+5*r);
+  }
+}
+
+function _dataLoader(dataBaseName,){
+  tableNames = loadTable(dataBaseName,"csv","header");
+  console.log("data loaded");
+  console.log(tableNames.getColumn("name"));
+}
+
+
+// Store your data.
+function saveStuff(obj) {
+  nameData.name = obj;
+  nameData.time = new Date().getTime();
+  // localStorage.nameData = JSON.stringify(nameData);
+  _emitter(nameData);
+}
+
+function loadStuff() {
+  return nameData.name || "default";
+}
+
+function _emitter(data){
+  socket.emit('nameSocket', data);
+  //console.log(data);
 }
